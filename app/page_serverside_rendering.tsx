3c1576a80d001c4ec747f7c003e46ae673946fba
andriy-shtumpf <shtumpf@gmail.com>
2024-11-04 12:45:02 -0500

    create car showroom

---
+++ app/page_serverside_rendering.tsx
@@ -0,0 +1,83 @@
+import {
+    CarCard,
+    CustomeFilter,
+    Hero,
+    SearchBar,
+    ShowMore,
+} from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
+import { FilterProps, HomeProps } from "@/types";
+import { fetchCars } from "@/utils";
+import { randomInt } from "crypto";
+import Image from "next/image";
+
+export default async function Home({ searchParams }: HomeProps) {
+    // const allCars = await fetchCars({
+    //     manufacturer: searchParams.manufacturer || "",
+    //     year: searchParams.year || 2022,
+    //     fuel: searchParams.fuel || "",
+    //     limit: searchParams.limit || 10,
+    //     model: searchParams.model || "",
+    // });
+
+    const params = await searchParams;
+    const allCars = await fetchCars({
+        manufacturer: params.manufacturer || "",
+        year: params.year || 2022,
+        fuel: params.fuel || "",
+        limit: params.limit || 10,
+        model: params.model || "",
+    });
+
+    console.log(allCars);
+
+    const isDataEmpty =
+        !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+    return (
+        <main className="overflow-hidden">
+            <Hero />
+
+            <div className="mt-12 padding-x padding-y max-width" id="discover">
+                <div className="home__text-container">
+                    <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+                    <p>Explore the cars you might like</p>
+                </div>
+
+                <div className="home__filters">
+                    <SearchBar />
+
+                    <div className="home__filter-container">
+                        <CustomeFilter title="fuel" options={fuels} />
+                        <CustomeFilter
+                            title="year"
+                            options={yearsOfProduction}
+                        />
+                    </div>
+                </div>
+
+                {!isDataEmpty ? (
+                    <section>
+                        <div className="home__cars-wrapper">
+                            {allCars?.map((car) => (
+                                <CarCard key={randomInt(9999999)} car={car} />
+                            ))}
+                        </div>
+
+                        <ShowMore
+                            pageNumber={(params.limit || 10) / 10}
+                            isNext={(params.limit || 10) > allCars.length}
+                        />
+                    </section>
+                ) : (
+                    <div className="home__error-container">
+                        <h2 className="text-black text-x1 font-bold">
+                            No results
+                        </h2>
+                        <p>{allCars?.message}</p>
+                    </div>
+                )}
+            </div>
+        </main>
+    );
+}
